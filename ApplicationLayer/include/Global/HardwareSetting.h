dd4dd0db38ff65703f58ed2e79e7d8820169b8b9
devonchenc <devonchenc@gmail.com>
2023-07-02 19:37:37 +1000

    Add comments for HardwareSetting.h

--- ApplicationLayer/include/Global/HardwareSetting.h
+++ ApplicationLayer/include/Global/HardwareSetting.h
@@ -7,8 +7,10 @@ class APPLICATIONLAYER_EXPORT HardwareSetting final
 public:
     static HardwareSetting& instance();
 
+    // 加载中心频率等系统参数
     void loadSetting();
 
+    // 返回中心频率
     double frequency() { return _frequency; }
 
     void setFrequency(double freq) { _frequency = freq; }
@@ -22,10 +24,12 @@ public:
     // 重新读取中心频率
     void reloadFrequency();
 
+    // 返回90度脉冲宽度
     double pw90() { return _pw90; }
 
     void setpw90(double pw90) { _pw90 = pw90; }
 
+    // 返回180度脉冲宽度
     double pw180() { return _pw180; }
 
     void setpw180(double pw180) { _pw180 = pw180; }
